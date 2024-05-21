6007c0603ff523a4b50f7dd8391dbf9eeb0ac40c
scorve12 <scorve5322@gamil.com>
2024-05-22 05:07:07 +0900

    feat 5guy

--- src/page/about.js
+++ src/page/about.js
@@ -1,11 +1,45 @@
 export default async function about() {
     return `
-    <div class='card'>
-        <img src='https://raw.githubusercontent.com/mohammadjarabah681/codepen-assets/main/pens/PoKWKGe/images/img.jpg' class='card__img'>
-        <div class='card__content'>
-            <h3 class='card__name'>Omar Ali</h3>
-            <h4 class='card__position'>Founder</h4>
-         </div>
+    <div class='warp'>
+        <div class='card'>
+            <img src='https://raw.githubusercontent.com/mohammadjarabah681/codepen-assets/main/pens/PoKWKGe/images/img.jpg' class='card__img'>
+            <div class='card__content'>
+                <h3 class='card__name'>김재호</h3>
+                <h4 class='card__position'>PM</h4>
+             </div>
+        </div>
+    
+        <div class='card'>
+            <img src='https://raw.githubusercontent.com/mohammadjarabah681/codepen-assets/main/pens/PoKWKGe/images/img.jpg' class='card__img'>
+            <div class='card__content'>
+                <h3 class='card__name'>강연준</h3>
+                <h4 class='card__position'>Back-End</h4>
+             </div>
+        </div>
+    
+        <div class='card'>
+            <img src='https://raw.githubusercontent.com/mohammadjarabah681/codepen-assets/main/pens/PoKWKGe/images/img.jpg' class='card__img'>
+            <div class='card__content'>
+                <h3 class='card__name'>최종빈</h3>
+                <h4 class='card__position'>Front-End</h4>
+             </div>
+        </div>
+    
+        <div class='card'>
+            <img src='https://raw.githubusercontent.com/mohammadjarabah681/codepen-assets/main/pens/PoKWKGe/images/img.jpg' class='card__img'>
+            <div class='card__content'>
+                <h3 class='card__name'>나우진</h3>
+                <h4 class='card__position'>Business</h4>
+             </div>
+        </div>
+    
+        <div class='card'>
+            <img src='https://raw.githubusercontent.com/mohammadjarabah681/codepen-assets/main/pens/PoKWKGe/images/img.jpg' class='card__img'>
+            <div class='card__content'>
+                <h3 class='card__name'>이도현</h3>
+                <h4 class='card__position'>Front-End</h4>
+             </div>
+        </div>
     </div>
     `;
 }
